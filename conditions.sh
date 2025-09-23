0162d85c8d8f24455cd4592edcae270b266fcb0a
smily kancharla <smilykancharla@gmail.com>
2025-09-23 14:27:04 +0530

    conditional statements

---
+++ conditions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+number=$1
+if [number -lt 10];then
+    echo "given number is less than 10:$number"
+elif [number -e 10];then
+    echo " given number is equal to 10"
+else
+    echo "given number is greater than 10:$number"
+
+fi
