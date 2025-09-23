f98017441905e040d6159248f69d4124f9510ece
smily kancharla <smilykancharla@gmail.com>
2025-09-23 14:49:46 +0530

    conditional statements2

--- conditions2.sh
+++ conditions2.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-a=10
+a=$1
 
-if [ $a -eq 10 ]; then  
-    echo "i am qual to 10"
+if [ $a -lt 10 ]; then  
+    echo "given number is leass than 10"
+elif [ $a -gt 10 ]; then
+    echo "given number is greater than 10 "
 else
-    echo "i am not "
+    echo "qual to 10"
 fi
