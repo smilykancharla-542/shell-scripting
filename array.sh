d2cfa963fc29e4de94396473a1a1932253d8df06
smily kancharla <smilykancharla@gmail.com>
2025-09-23 12:02:52 +0530

    array

--- array.sh
+++ array.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-leaders=("modi","trump")
+leaders=("modi" "trump")
 
-echo "india leader is: ${leaders[0]}"
+echo "india leader is:${leaders[0]}"
 
-echo "us leader is : ${leaders[1]}"
+echo "us leader is :${leaders[1]}"
 
 echo "all leaders:${leaders[@]}"
